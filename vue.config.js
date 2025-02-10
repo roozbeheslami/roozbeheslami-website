781c01c6289bdba987f2a933154fdb3411beec85
Roozbeh Eslami <eslami.roozbeh@gmail.com>
2025-02-10 16:13:21 +0330

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	publicPath: "",
+  publicPath: process.env.NODE_ENV === "production" ? "/roozbeheslami-website/" : "/",
 	outputDir: path.resolve(__dirname, './docs'),
 	chainWebpack: config => {
 		config.plugin("html").tap(args => {
