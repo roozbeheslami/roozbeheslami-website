0f71dd040f325a83cbbdc081a47acf7316c049ea
Roozbeh Eslami <eslami.roozbeh@gmail.com>
2025-02-10 15:38:46 +0330

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	publicPath: "",
-	outputDir: path.resolve(__dirname, './docs'),
+	outputDir: path.resolve(__dirname, './dist'),
 	chainWebpack: config => {
 		config.plugin("html").tap(args => {
 			args[0].title = "Roozbeh Eslami";
